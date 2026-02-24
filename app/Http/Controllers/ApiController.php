7b8c1286e9b2c6e11b9d43ed7439e1526d4cc087
subhomoy samanta <subhomoy@keylines.net>
2026-02-24 12:41:26 +0530

    api implement

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1158,7 +1158,7 @@ class ApiController extends Controller
                     $getUser    = User::where('id', '=', $uId)->first();
                     if($getUser){
                         $member = User::findOrFail($uId);
-                        Helper::pr($getUser);
+                        // Helper::pr($getUser);
                         /** Photo Update */
                         if ($request->hasFile('photo')) {
                             $oldPath = public_path('uploads/user/'.$member->photo);
