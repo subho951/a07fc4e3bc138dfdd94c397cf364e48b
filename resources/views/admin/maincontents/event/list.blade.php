2f8a3a6d964c60607715e79ac678ac053240b8bc
subhomoy samanta <subhomoy@keylines.net>
2026-02-27 20:59:15 +0530

    event data & ;2~image upload

--- resources/views/admin/maincontents/event/list.blade.php
+++ resources/views/admin/maincontents/event/list.blade.php
@@ -54,8 +54,8 @@ $controllerRoute = $module['controller_route'];
                   <th scope="row"><?=$sl++?></th>
                   <td><?=$row->title?></td>
                   <td><?=$row->venue?></td>
-                  <td><?=$row->event_date?></td>
-                  <td><?=$row->event_time?></td>
+                  <td><?=date_format(date_create($row->event_date), "d-m-Y")?></td>
+                  <td><?=date_format(date_create($row->event_time), "h:i A")?></td>
                   <td>
                     <?php
                     echo $questionCount = EventQuestion::where('event_id', '=', $row->id)->count();
