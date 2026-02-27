cd385a678934631c96fd71b906e0340cacae317a
subhomoy samanta <subhomoy@keylines.net>
2026-02-27 14:00:27 +0530

    events & event details api updated

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1509,7 +1509,7 @@ class ApiController extends Controller
                                     'title'             => $row->title,
                                     'description'       => $row->description,
                                     'venue'             => $row->venue,
-                                    'event_date'        => date_format(date_create($row->event_date), "F M d") . ' . ' . date_format(date_create($row->event_time), "h:i A"),
+                                    'event_date'        => date_format(date_create($row->event_date), "l, M d Y") . ' . ' . date_format(date_create($row->event_time), "h:i A"),
                                     'photo'             => (($row->photo != '')?env('UPLOADS_URL').'event/'.$row->photo:env('NO_IMAGE')),
                                 ];
                             }
