c739bc4d6c1883e8bae0bf5137a790b8e48e7dfa
subhomoy samanta <subhomoy@keylines.net>
2026-03-07 20:32:50 +0530

    firebase implement

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2359,15 +2359,16 @@ class ApiController extends Controller
                         } else {
                             $offset = (($limit * $page_no) - $limit); // ((15 * 3) - 15)
                         }
-                        $notifications    = Notification::select('description')
-                                                        ->where('to_users', $uId)
-                                                        ->where('status', 1)
-                                                        ->where('is_send', 1)
-                                                        ->groupBy('description')
-                                                        ->orderBy('id', 'DESC')
-                                                        ->offset($offset)
-                                                        ->limit($limit)
-                                                        ->get();
+                        // $notifications    = Notification::select('id', 'title', 'description', 'send_timestamp', 'users')->where('to_users', '=', $uId)->where('status', '=', 1)->where('is_send', '=', 1)->orderBy('id', 'DESC')->offset($offset)->limit($limit)->get();
+                        $notifications    = Notification::select('id', 'title', 'description', 'send_timestamp', 'users')
+                                                            ->where('to_users', $uId)
+                                                            ->where('status', 1)
+                                                            ->where('is_send', 1)
+                                                            ->groupBy('description')
+                                                            ->orderBy('id', 'DESC')
+                                                            ->offset($offset)
+                                                            ->limit($limit)
+                                                            ->get();
                         if($notifications){
                             foreach($notifications as $notification){
                                 $users = json_decode($notification->users);
