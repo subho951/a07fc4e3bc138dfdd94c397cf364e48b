8b10b253c90a1db9efb469c05e46ce0cb8980cfe
subhomoy samanta <subhomoy@keylines.net>
2026-03-14 21:04:50 +0530

    event images updated

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2549,7 +2549,7 @@ class ApiController extends Controller
                             $data['generalSetting']             = GeneralSetting::find('1');
                             $data['member']                     = $getUser;
                             $html                               = view('front.identity-card-v2', $data)->render();
-                            echo $html;die;
+                            // echo $html;die;
                             $apiResponse['html']                = $html;
 
                             $apiStatus                          = TRUE;
